4a4aaa92af36d42b4f7d6df48f3010b1e788d111
Eric Faris <ericfaris@gmail.com>
2021-06-13 00:09:42 -0400

    setting up correct package.json dependencies and index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ping Pong Bot Loaded!');
+});
+
+client.on('message', message => {
+	if (message.content === 'Ping') {
+		message.channel.send('Pong!');
+	}
+});
+
+client.login('');
