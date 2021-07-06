77d78efd6e4ff17d50ab3c00e0b1b7881c605047
Eric Faris <ericfaris@gmail.com>
2021-07-05 23:40:00 -0400

    removing test servers

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = new DiscordJS.Client()
 client.on('ready', () => {
   new WOKCommands(client, {
     commandsDir: process.env.COMMANDS_DIR,
-    testServers: [process.env.ERICFARIS_TEST_SERVER_ID],
+    // testServers: [process.env.ERICFARIS_TEST_SERVER_ID],
     showWarns: false,
     del: 4
   })
