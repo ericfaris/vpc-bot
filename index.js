4623bcf84f9ed0697eb520b111f3cc1297a69d8b
Eric Faris <ericfaris@gmail.com>
2021-07-22 00:58:49 -0400

    forgot to turn off test servers

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = new DiscordJS.Client()
 client.on('ready', () => {
   new WOKCommands(client, {
     commandsDir: process.env.COMMANDS_DIR,
-    testServers: [process.env.GUILD_ID],
+    // testServers: [process.env.GUILD_ID],
     showWarns: false,
     del: process.env.SECONDS_TO_DELETE_MESSAGE
   })
