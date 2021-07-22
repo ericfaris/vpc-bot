7e76b4143e86dc9c9186b10814a8c2b17b029686
Eric Faris <ericfaris@gmail.com>
2021-07-22 16:18:56 -0400

    adding ability for the bot to know what environment it is running in

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ const WOKCommands = require('wokcommands')
 require('dotenv').config()
 
 const client = new DiscordJS.Client()
+const PRODUCTION = 'production';
+const TEST = 'test';
 
 client.on('ready', () => {
-  new WOKCommands(client, {
-    commandsDir: process.env.COMMANDS_DIR,
-    // testServers: [process.env.GUILD_ID],
-    showWarns: false,
-    del: process.env.SECONDS_TO_DELETE_MESSAGE
-  })
+  if (process.env.ENVIRONMENT === this.PRODUCTION) {
+    new WOKCommands(client, {
+      commandsDir: process.env.COMMANDS_DIR,
+      showWarns: false,
+      del: process.env.SECONDS_TO_DELETE_MESSAGE
+    })
+  } else {
+    new WOKCommands(client, {
+      commandsDir: process.env.COMMANDS_DIR,
+      testServers: [process.env.GUILD_ID],
+      showWarns: false,
+      del: process.env.SECONDS_TO_DELETE_MESSAGE
+    })
+  }
 })
 
 client.login(process.env.BOT_TOKEN)
