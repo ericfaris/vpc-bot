dc37770029d9613aa0d0baf72fb3660cb3c97268
Eric Faris <ericfaris@gmail.com>
2021-08-13 18:28:01 -0400

    changing point schedule

--- helpers/scoreHelper.js
+++ helpers/scoreHelper.js
@@ -25,28 +25,44 @@ module.exports = {
     modifyPoints: (scores) => {
 
         if(scores.length > 0) {
-            scores[0].points = 7;
+            scores[0].points = 12;
         }
 
         if(scores.length > 1) {
-            scores[1].points = 5;
+            scores[1].points = 10;
         }
 
         if(scores.length > 2) {
-            scores[2].points = 3;
+            scores[2].points = 9;
         }
 
         if(scores.length > 3) {
-            scores[3].points = 2;
+            scores[3].points = 8;
         }
 
         if(scores.length > 4) {
-            scores[4].points = 1;
+            scores[4].points = 7;
         }
 
         if(scores.length > 5) {
-            for (let index = 5; index < scores.length; index++) {
-                scores[index].points = 0;
+            scores[5].points = 6;
+        }
+
+        if(scores.length > 6) {
+            scores[6].points = 5;
+        }
+
+        if(scores.length > 7) {
+            scores[7].points = 4;
+        }
+
+        if(scores.length > 8) {
+            scores[8].points = 3;
+        }
+
+        if(scores.length > 9) {
+            for (let index = 9; index < scores.length; index++) {
+                scores[index].points = 1;
             }
         }
     },
