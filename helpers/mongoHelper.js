e1fb216a4486c1ddb9941dad001d20bd3922546b
Eric Faris <ericfaris@gmail.com>
2021-10-24 23:58:16 -0400

    initial switch to mongoDB

---
+++ helpers/mongoHelper.js
@@ -0,0 +1,21 @@
+const {MongoClient} = require('mongodb');
+
+module.exports = {
+
+    connect: async (dbName) => {
+        const uri = `mongodb+srv://vpchat:***REMOVED***@cluster0.blwxx.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        return await client.connect();
+    },
+    
+    getAll: async (collectionName) => {
+        let findResult;
+
+        const client = await module.exports.connect();
+        const collection = client.db("vpc").collection(collectionName);      
+        findResult = await collection.find({}).toArray();
+        client.close();
+
+        return findResult;
+    },
+}
