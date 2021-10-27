58aa6d3119b52367256a9b692fbb9c629d02c461
Eric Faris <ericfaris@gmail.com>
2021-10-26 23:12:30 -0400

    remove console.log statement

--- helpers/permissionHelper.js
+++ helpers/permissionHelper.js
@@ -5,7 +5,6 @@ module.exports = {
     hasPermissionOrRole: async (client, interaction, permissions, roles) =>{
         const guild = await client.guilds.fetch(process.env.GUILD_ID);
         const member = await guild.members.fetch(interaction.member.user.id);
-        console.log(member.roles);
         const hasRole = member.roles.cache.some(role => roles?.indexOf(role.name) > -1);
         
         return member.hasPermission(permissions) || hasRole;
