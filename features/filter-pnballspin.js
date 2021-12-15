e87b6b7f9a9eadba43bf22bf729e8e2a656be8b4
Eric Faris <ericfaris@gmail.com>
2021-12-15 00:37:21 -0500

    added filter-pnballspin feature

---
+++ features/filter-pnballspin.js
@@ -0,0 +1,14 @@
+module.exports = (client, instance) => {
+
+    client.on("messageCreate", (message) => {
+      const { channel, author } = message
+      const OFFENDING_USER = 'ericfaris';
+      let regexp = /^\b[a-zA-Z0-9_]+\b$/gi;
+    //   let regexp = /^\b[a-zA-Z0-9_]+\b$|^\b[a-zA-Z0-9_]+\b \b[a-zA-Z0-9_]+\b$/gi;
+
+      if(author.username === OFFENDING_USER && message.content.match(regexp)) {
+        message.delete();
+        author.send('Message deleted as not relevant.');
+      }
+    })
+}
--- index.js
+++ index.js
@@ -37,6 +37,8 @@ const client = new DiscordJS.Client({
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
   ],
 })
 
