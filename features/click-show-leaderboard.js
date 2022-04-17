2bd08261141021dd55a56d93bc9776dd2ccbe771
Eric Faris <ericfaris@gmail.com>
2022-04-16 20:55:20 -0400

    corrected the operator for equal check

--- features/click-show-leaderboard.js
+++ features/click-show-leaderboard.js
@@ -2,7 +2,7 @@ const showLeaderboardCommand = require('../commands/show-leaderboard');
 
 module.exports = (client) => {
   client.on('interactionCreate', async interaction => {
-    if (!interaction.customId != 'showLeaderboard') return;
+    if (interaction.customId != 'showLeaderboard') return;
     await showLeaderboardCommand.getLeaderboard(interaction, interaction.channel);        
   });
 }
