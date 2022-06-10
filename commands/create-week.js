db1e42c317ac02af2f60742b2d59f50bf8b14c23
Eric Faris <ericfaris@gmail.com>
2022-06-10 01:12:12 -0400

    added romrequired parameter for creating new week.

--- commands/create-week.js
+++ commands/create-week.js
@@ -16,8 +16,8 @@ module.exports = {
   hidden: true,
   description: 'Create new week by VPS ID (MANAGE_GUILD)',
   roles: ['Competition Corner Mod'],
-  minArgs: 1,
-  expectedArgs: '<vpsid> <startdateoverride> <enddateoverride> <b2sidoverride> <notes>',
+  minArgs: 2,
+  expectedArgs: '<vpsid> <romrequired> <startdateoverride> <enddateoverride> <b2sidoverride> <notes>',
   callback: async ({ args, client, channel, interaction, instance, message}) => {
     let retVal;
     let ephemeral = false;
@@ -35,7 +35,8 @@ module.exports = {
       retVal = `The ${module.exports.commandName} slash command cannot be used in this channel.`;
       ephemeral = true;
     } else {
-      const [vpsid, startdateoverride, enddateoverride, b2sidoverride, notes] = args;
+      const [vpsid, romrequired, startdateoverride, enddateoverride, b2sidoverride, notes] = args;
+      let errors = [];
       let weekNumber;
       let periodStart;
       let periodEnd;
@@ -52,7 +53,7 @@ module.exports = {
       if (vpsGame.table) {
         const currentSeason = await mongoHelper.findCurrentSeason(channel.name);
         const currentWeek = await vpcDataService.getCurrentWeek(channel.name);
-      
+              
         weekNumber = parseInt(currentWeek.weekNumber) + 1;
         currentSeasonWeekNumber = parseInt(currentWeek.currentSeasonWeekNumber) + 1;
         periodStart = startdateoverride ?? date.format(date.addDays(date.parse(currentWeek.periodStart, 'YYYY-MM-DD'), 7), 'YYYY-MM-DD');
@@ -61,61 +62,67 @@ module.exports = {
         authorName = vpsGame.table?.authors?.join(', ') ?? '';
         versionNumber = vpsGame?.table?.version ?? '';
         tableUrl = vpsGame.table?.urls[0]?.url ?? '';
-        romUrl = vpsGame?.romFiles ? vpsGame?.romFiles[0]?.urls[0]?.url ?? '' : '';
-        romName = vpsGame?.romFiles ? vpsGame?.romFiles[0]?.version ?? '' : '';
+        romUrl = vpsGame?.romFiles.length > 0 ? (vpsGame?.romFiles[0]?.urls.length > 0 ? vpsGame?.romFiles[0].urls[0]?.url ?? '' : '') : '';
+        romUrl === '' ? errors.push('romUrl not found on VPS.  Please update VPS with at least 1 romUrl.') : '';
+        romName = vpsGame?.romFiles.length > 0 ? (vpsGame?.romFiles[0]?.version ?? '') : '';
+        romName === '' ? errors.push('romName not found on VPS.  Please update VPS with at least 1 romName.') : '';
         b2sUrl = b2sidoverride ? vpsGame?.b2sFiles.find(b => b.id === b2sidoverride)?.urls[0]?.url : (vpsGame?.b2sFiles[0]?.urls[0]?.url ?? '') ;
 
-        var newWeek = {
-          'channelName': channel.name,
-          'weekNumber': weekNumber,
-          'periodStart': periodStart,
-          'periodEnd': periodEnd,
-          'table': table,
-          'authorName': authorName,
-          'versionNumber': versionNumber,
-          'vpsId': vpsid,
-          'tableUrl': tableUrl,
-          'romUrl': romUrl,
-          'romName': romName,
-          'b2sUrl': b2sUrl,
-          'season': currentSeason?.seasonNumber ? parseInt(currentSeason?.seasonNumber) : null,
-          'currentSeasonWeekNumber': currentSeasonWeekNumber,
-          'notes': notes,
-          'scores': [],
-          'teams': [],
-          'isArchived': false
-        }
-
-        await mongoHelper.updateOne({ channelName: channel.name, isArchived: false }, { $set: { isArchived: true } }, null, 'weeks');
-        await mongoHelper.insertOne(newWeek, 'weeks');
-
-        if (channel.name === process.env.COMPETITION_CHANNEL_NAME) {
-          await outputHelper.editWeeklyCompetitionCornerMessage(newWeek.scores, client, newWeek, newWeek.teams);
-
-          if(currentSeason) {
-            const weeksInSeason = await mongoHelper.find({
-              channelName: channel.name,
-              isArchived: true,
-              periodStart: { $gte: currentSeason.seasonStart },
-              periodEnd: { $lte: currentSeason.seasonEnd }
-            }, 'weeks');
-
-            await outputHelper.editSeasonCompetitionCornerMessage(currentSeason, weeksInSeason, client)
+        if(errors.length === 0 || !romrequired) {
+          var newWeek = {
+            'channelName': channel.name,
+            'weekNumber': weekNumber,
+            'periodStart': periodStart,
+            'periodEnd': periodEnd,
+            'table': table,
+            'authorName': authorName,
+            'versionNumber': versionNumber,
+            'vpsId': vpsid,
+            'tableUrl': tableUrl,
+            'romUrl': romUrl,
+            'romName': romName,
+            'b2sUrl': b2sUrl,
+            'season': currentSeason?.seasonNumber ? parseInt(currentSeason?.seasonNumber) : null,
+            'currentSeasonWeekNumber': currentSeasonWeekNumber,
+            'notes': notes,
+            'scores': [],
+            'teams': [],
+            'isArchived': false
           }
 
-          retVal = `New week created and the ${process.env.COMPETITION_CHANNEL_NAME} pinned message was updated successfully.`;
+          await mongoHelper.updateOne({ channelName: channel.name, isArchived: false }, { $set: { isArchived: true } }, null, 'weeks');
+          await mongoHelper.insertOne(newWeek, 'weeks');
+
+          if (channel.name === process.env.COMPETITION_CHANNEL_NAME) {
+            await outputHelper.editWeeklyCompetitionCornerMessage(newWeek.scores, client, newWeek, newWeek.teams);
+
+            if(currentSeason) {
+              const weeksInSeason = await mongoHelper.find({
+                channelName: channel.name,
+                isArchived: true,
+                periodStart: { $gte: currentSeason.seasonStart },
+                periodEnd: { $lte: currentSeason.seasonEnd }
+              }, 'weeks');
+
+              await outputHelper.editSeasonCompetitionCornerMessage(currentSeason, weeksInSeason, client)
+            }
+
+            retVal = `New week created and the ${process.env.COMPETITION_CHANNEL_NAME} pinned message was updated successfully.`;
+          } else {
+            retVal = `New week created for the ${channel.name} channel.`;
+          }
+    
+          await commandHelper.execute(instance, interaction, message, 'create-high-score-table', [newWeek.vpsId]);
+
+          if (channel.name === process.env.COMPETITION_CHANNEL_NAME) {
+            client.emit('advancePlayoffRound', channel, currentWeek);
+            client.emit('postBraggingRights', process.env.BRAGGING_RIGHTS_CHANNEL_ID, currentWeek);
+          }  
+
+          interaction.editReply({content: retVal, ephemeral: ephemeral});
         } else {
-          retVal = `New week created for the ${channel.name} channel.`;
+          interaction.editReply({content: `***Error creating new week. New week HAS NOT been created***: \n\n${errors.join('\n')}`, ephemeral: ephemeral});
         }
-  
-        await commandHelper.execute(instance, interaction, message, 'create-high-score-table', [newWeek.vpsId]);
-
-        if (channel.name === process.env.COMPETITION_CHANNEL_NAME) {
-          client.emit('advancePlayoffRound', channel, currentWeek);
-          client.emit('postBraggingRights', process.env.BRAGGING_RIGHTS_CHANNEL_ID, currentWeek);
-        }  
-
-        interaction.editReply({content: retVal, ephemeral: ephemeral});
       } else {
         retVal = `No VPS Tables were found.  Please double check your VPS ID.`;
       }
