b5604f5c718edc8fdaa3533fb47aa1ef77fd90fe
Eric Faris <ericfaris@gmail.com>
2022-06-13 11:00:04 -0400

    adjusting playoff check

--- features/advance-playoff-round.js
+++ features/advance-playoff-round.js
@@ -6,39 +6,42 @@ module.exports = (client, channel) => {
     const playoffHelper = new PlayoffHelper();
 
     const currentPlayoff = await mongoHelper.findCurrentPlayoff(channel.name);
-    const currentPlayoffRound = await mongoHelper.findCurrentPlayoffRound(channel.name);
 
-    const games = playoffHelper.getCurrentPlayoffMatchups(currentWeek, currentPlayoff, currentPlayoffRound);
-    const winningSeeds = playoffHelper.findWinningSeeds(games);
+    if(currentPlayoff) {
+      const currentPlayoffRound = await mongoHelper.findCurrentPlayoffRound(channel.name);
 
-    switch(winningSeeds.length) {
-      case 16:
-        roundName = '1st Round';
-        break;
-      case 8:
-        roundName = '2nd Round';
-        break;
-      case 4:
-        roundName = 'Semifinal Round';
-        break;
-      case 2:
-        roundName = 'Championship Round';
-        break;
+      const games = playoffHelper.getCurrentPlayoffMatchups(currentWeek, currentPlayoff, currentPlayoffRound);
+      const winningSeeds = playoffHelper.findWinningSeeds(games);
+
+      switch(winningSeeds.length) {
+        case 16:
+          roundName = '1st Round';
+          break;
+        case 8:
+          roundName = '2nd Round';
+          break;
+        case 4:
+          roundName = 'Semifinal Round';
+          break;
+        case 2:
+          roundName = 'Championship Round';
+          break;
+      }
+
+      const currentSeason = await mongoHelper.findOne({ channelName: channel.name, isArchived: false }, 'seasons');
+      await mongoHelper.updateOne({ seasonNumber: parseInt(currentSeason.seasonNumber), isArchived: false }, { $set: { isArchived: true } }, null, 'rounds');
+
+      let round = {
+        'channelName' : channel.name,
+        'seasonNumber' : parseInt(currentSeason.seasonNumber),
+        'roundName' : roundName,
+        'games' : winningSeeds,
+        'isArchived' : false
+      }
+
+      await mongoHelper.insertOne(round, 'rounds');
+
+      await channel.send({content: `Advanced playoff round.`});
     }
-
-    const currentSeason = await mongoHelper.findOne({ channelName: channel.name, isArchived: false }, 'seasons');
-    await mongoHelper.updateOne({ seasonNumber: parseInt(currentSeason.seasonNumber), isArchived: false }, { $set: { isArchived: true } }, null, 'rounds');
-
-    let round = {
-      'channelName' : channel.name,
-      'seasonNumber' : parseInt(currentSeason.seasonNumber),
-      'roundName' : roundName,
-      'games' : winningSeeds,
-      'isArchived' : false
-    }
-
-    await mongoHelper.insertOne(round, 'rounds');
-
-    await channel.send({content: `Advanced playoff round.`});
   });
 }
