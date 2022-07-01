dbeef7fd0229ab8017408cac4b9e4bb248cd4954
Eric Faris <ericfaris@gmail.com>
2022-06-30 23:47:04 -0400

    refactored for permissionHelper

--- commands/create-playoff.js
+++ commands/create-playoff.js
@@ -1,8 +1,7 @@
 require('dotenv').config()
 const path = require('path');
-const date = require('date-and-time');
 const outputHelper = require('../helpers/outputHelper');
-const permissionHelper = require('../helpers/permissionHelper');
+const { PermissionHelper2 } = require('../helpers/permissionHelper2');
 const mongoHelper = require('../helpers/mongoHelper');
 
 module.exports = {
@@ -11,22 +10,23 @@ module.exports = {
   testOnly: true,
   guildOnly: true,
   hidden: true,
-  description: 'Create new playoff using Season # and comma seperated list of Seed#:Username',
-  roles: ['Competition Corner Mod'],
-  // minArgs: 1,
-  // expectedArgs: '<seasonnumber> <seedanduserlist>',
+  description: 'Creates new playoff from current season leaderboard.',
+  roles: [process.env.BOT_CONTEST_ADMIN_ROLE_NAME],
+  channels: [process.env.COMPETITION_CHANNEL_NAME],
   callback: async ({ args, client, channel, interaction, instance }) => {
+    let permissionHelper2 = new PermissionHelper2();
     let retVal;
     let ephemeral = false;
 
-    if (!(await permissionHelper.hasRole(client, interaction, module.exports.roles))) {
-      console.log(`${interaction.member.user.username} DOES NOT have the correct role or permission to run ${module.exports.commandName}.`);
-      retVal = `The ${module.exports.commandName} slash command can only be executed by an admin.`;
-      ephemeral = true;
-    } else if (channel.name !== process.env.COMPETITION_CHANNEL_NAME) {
-      retVal = `The ${module.exports.commandName} slash command cannot be used in this channel.`;
-      ephemeral = true;
-    } else {
+    // Check if the User has a valid Role
+    retVal = await permissionHelper2.hasRole(client, interaction, module.exports.roles, module.exports.commandName);
+    if (retVal) {interaction.reply({content: retVal, ephemeral: true}); return;}
+
+    // Check if the Channel is valid
+    retVal = await permissionHelper2.isValidChannel(module.exports.channels, interaction, module.exports.commandName);
+    if (retVal) {interaction.reply({content: retVal, ephemeral: true}); return;}
+
+    try {
       const currentSeason = await mongoHelper.findOne({ channelName: channel.name, isArchived: false }, 'seasons');
 
       leaderboard = (await module.exports.getCurrentSeasonLeaderboard(channel)).slice(0,16);
@@ -40,6 +40,9 @@ module.exports = {
       await mongoHelper.insertOne(playoff, 'playoffs');
 
       return "Playoff created."
+    } catch(error) {
+      logger.error(error.message);
+      interaction.reply({content: error.message, ephemeral: ephemeral});
     }
   },
 
@@ -54,6 +57,5 @@ module.exports = {
 
     return outputHelper.getSeasonLeaderboard(weeks);
   }
-
 }
  
