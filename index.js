16c01967c19fe5223613929852b17fab38c3f1ff
Eric Faris <ericfaris@gmail.com>
2022-07-02 09:47:48 -0400

    adding new variables to printout on startup

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ let logger = (new Logger(null)).logger;
 
 logger.info('Starting bot');
 logger.info(`BOT_TOKEN: ${process.env.BOT_TOKEN}`);
+logger.info(`BOT_USER: ${process.env.BOT_USER}`);
+logger.info(`BOT_CONTEST_ADMIN_ROLE_NAME: ${process.env.BOT_CONTEST_ADMIN_ROLE_NAME}`);
+logger.info(`BOT_HIGH_SCORE_ADMIN_ROLE_NAME: ${process.env.BOT_HIGH_SCORE_ADMIN_ROLE_NAME}`);
 logger.info(`COMMANDS_DIR: ${process.env.COMMANDS_DIR}`);
 logger.info(`FEATURES_DIR: ${process.env.FEATURES_DIR}`);
 logger.info(`COMPETITION_CHANNEL_NAME: ${process.env.COMPETITION_CHANNEL_NAME}`);
@@ -29,6 +32,8 @@ logger.info(`VPC_DATA_SERVICE_API_URI: ${process.env.VPC_DATA_SERVICE_API_URI}`)
 logger.info(`VPS_DATA_SERVICE_API_URI: ${process.env.VPS_DATA_SERVICE_API_URI}`);
 logger.info(`BRAGGING_RIGHTS_CHANNEL_NAME: ${process.env.BRAGGING_RIGHTS_CHANNEL_NAME}`);
 logger.info(`BRAGGING_RIGHTS_CHANNEL_ID: ${process.env.BRAGGING_RIGHTS_CHANNEL_ID}`);
+logger.info(`CHANNELS_WITH_SCORES: ${process.env.CHANNELS_WITH_SCORES}`);
+logger.info(`CONTEST_CHANNELS: ${process.env.CONTEST_CHANNELS}`);
 
 const client = new DiscordJS.Client({
   intents: [
