630c22a02c2fcc948f924fc35cbc2ca9a58a3ca5
Eric Faris <ericfaris@gmail.com>
2022-07-22 00:13:49 -0400

    fixed bug

--- helpers/permissionHelper.js
+++ helpers/permissionHelper.js
@@ -8,7 +8,7 @@ class PermissionHelper {
     async hasRole(client, interaction, roles, commandName) {
         let logger = (new Logger(interaction?.member?.user)).logger;
         const guild = await client.guilds.fetch(process.env.GUILD_ID);
-        const member = await guild.members.fetch(interaction.member.user.id);
+        const member = await guild.members.fetch(interaction?.member?.user?.id);
         const hasRole = [...member.roles.cache.values()].some(value => roles?.indexOf(value?.name) > -1);
         logger.info(`roles: ${roles.join(', ')}`);
         logger.info(`hasRole: ${hasRole}}`);
@@ -18,8 +18,8 @@ class PermissionHelper {
 
     async isValidChannel(channels, interaction, commandName) {
         let logger = (new Logger(interaction?.member?.user)).logger;
-        const isValidChannel = channels.includes(interaction.channel.name);
-        logger.info(`channelName: ${interaction.channel.name}`);
+        const isValidChannel = channels.includes(interaction?.channel?.name);
+        logger.info(`channelName: ${interaction?.channel?.name}`);
         logger.info(`isValidChannel: ${isValidChannel}}`);
 
         return (!isValidChannel ? `The ${commandName} slash command cannot be used in this channel.` : null);
